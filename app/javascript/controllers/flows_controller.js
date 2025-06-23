890f16ee91b9d53e66ac72bda31d0566f3ab5939
m-deura <aw.mayuki.deura@gmail.com>
2025-06-23 15:46:00 +0900

    add: cytoscape-dagreを導入

--- app/javascript/controllers/flows_controller.js
+++ app/javascript/controllers/flows_controller.js
@@ -1,6 +1,9 @@
 // app/javascript/controllers/flow_controller.js
-import { Controller } from "@hotwired/stimulus"
-import cytoscape from "cytoscape"
+import { Controller } from "@hotwired/stimulus";
+import cytoscape from "cytoscape";
+import dagre from "cytoscape-dagre";
+
+cytoscape.use(dagre);
 
 export default class extends Controller {
   connect() {
@@ -10,51 +13,54 @@ export default class extends Controller {
         { data: { id: "closed", label: "クローズドガード" } },
         { data: { id: "kusakari", label: "草刈り" } },
         { data: { id: "pass", label: "パスガード" } },
-        { data: { id: "sample1", label: "サンプル1" }},
+        { data: { id: "sample1", label: "サンプル1" } },
+        { data: { id: "sample2", label: "サンプル2" } },
+        { data: { id: "sample3", label: "サンプル3" } },
+        { data: { id: "sample4", label: "サンプル4" } },
+        { data: { id: "sample5", label: "サンプル5" } },
         { data: { source: "closed", target: "kusakari" } },
         { data: { source: "kusakari", target: "pass" } },
-        { data: { source: "kusakari", target: "sample1" }}
+        { data: { source: "kusakari", target: "sample1" } },
+        { data: { source: "kusakari", target: "sample2" } },
+        { data: { source: "closed", target: "sample3" } },
+        { data: { source: "sample3", target: "sample4" } },
+        { data: { source: "sample3", target: "sample5" } },
       ],
       style: [
         {
           selector: "node",
           style: {
-      "shape": "roundrectangle",
-      "width": "label",            // ← 非推奨？要確認
-      "height": 30,
-      "background-color": "#FFFFFF",
-      "label": "data(label)",
-      "text-valign": "center",
-      "text-halign": "center",
-      "color": "#1E3A8A",
-      "font-size": "14px",
-      "padding": "4px"
-          }
+            shape: "roundrectangle",
+            width: "label", // ← 非推奨？要確認
+            height: 10,
+            "background-color": "#FFFFFF",
+            label: "data(label)",
+            "text-valign": "center",
+            "text-halign": "center",
+            // "color": "#1E3A8A",
+            "font-size": "8px",
+            padding: "4px",
+          },
         },
         {
           selector: "edge",
           style: {
-            "background-color": "#1E3A8A",
-            "width": 2,
-            "line-color": "#1E3A8A",
+            "curve-style": "round-taxi",
+            "width": 1,
+            "line-color": "#4984dd",
             "target-arrow-shape": "none",
-            "curve-style": "bezier"
-          }
-        }
+            "taxi-radius": 50,
+            "taxi-turn": "10px", // 曲がり角の位置として、ソースノードからの絶対距離を指定する（指定しないとソースノードとターゲットノードの相対距離によって位置算出が行われるらしく、同列ノードの曲がり角の位置がズレる）
+          },
+        },
       ],
       layout: {
-          name: "grid",
-  rows: 1,
-  avoidOverlap: true,
-  spacingFactor: 2
-        // name: "preset", // 手動で位置指定
-        // positions: {
-        //   closed: { x: 100, y: 100 },
-        //   kusakari: { x: 250, y: 100 },
-        //   pass: { x: 400, y: 100 },
-        //   sample1: { x: 400, y: 150 }
-        // }
-      }
-    })
+        name: "dagre",
+        rankDir: "LR",
+        nodeSep: 0,
+        edgeSep: 10,
+        ranker: "tight-tree",
+      },
+    });
   }
 }
