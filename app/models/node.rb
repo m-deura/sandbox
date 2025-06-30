ee847d576c09e014211fc9bd6aeb69b67af42bf3
m-deura <aw.mayuki.deura@gmail.com>
2025-06-30 15:02:50 +0900

    update: revised the model files

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,5 @@
 class Node < ApplicationRecord
-  belongs_to :action_id
+  belongs_to :action
+  has_many :outgoing_edges, class_name: "Node", foreign_key: "source_node_id", dependent: :destroy
+  has_many :incoming_edges, class_name: "Edge", foreign_key: "source_node_id", dependent: :destroy
 end
